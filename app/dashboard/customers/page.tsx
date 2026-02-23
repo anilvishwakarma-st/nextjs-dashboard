8aaed7dc23a314440416228089b1b82e20bec4cb
Inno <inno@LAP-GET-21.local>
2026-02-23 15:42:33 +0530

    app router project completed from the oficial next docs

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,26 @@
-export default function Page() {
-  return <p>Customers Page inside dashboard</p>;
+import { Metadata } from "next";
+
+async function fetchCustomers() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  return ["John", "Alice", "Bob"];
+}
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+
+  return (
+    <div>
+      <h1>Customers</h1>
+      <ul>
+        {customers.map((c) => (
+          <li key={c}>{c}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
